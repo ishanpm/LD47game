7894f3686233a3dadecb77956a4db833a2853237
anjupear <72323835+anjupear@users.noreply.github.com>
2020-10-05 17:30:45 -0500

    Create convos.js

---
+++ convos.js
@@ -0,0 +1,18 @@
+labConv: [
+      {who: "player", say:"I must ask, miss, have you seen anything strange in town? Any unusual occurrences?"},
+      {who: "lab", say:"Well, there’s a kid who spends hours in the park next door late at night every now and then. No idea what she’s up to."},
+      {who: "player", say: "I thanked the dame for her time and went on my way."},
+      {who: "lab", say: "I’m a black lab, actually."},
+      {who: "player", say: "No, not Dane, dame… am I talking out loud?"}
+      ]
+bouncerConv1: [
+	{who: "bouncer", say:"VIP members only, bud. Scram."}
+]
+
+bouncerConv2: [
+	{who: "bouncer", say:"VIP members only, bud. Scram."},
+	{who: "player", say:"That’s no way to talk to your boss."},
+	{who: "bouncer", say:"Wha- Mr. Smith?"},
+	{who: "player", say:"The one and only, Brutus. Now let me in, I need to talk to the boss."},
+	{who: "bouncer", say:"Yes, sir!"},
+]
